c22ca4281cf4fbb8fe34ca0994283caa2a5f8800
Malik <malik.hassanaly@gmail.com>
2023-07-20 17:30:56 -0600

    avoid repetition of mech name

--- Support/ceptr/script.sh
+++ Support/ceptr/script.sh
@@ -12,15 +12,16 @@ module purge
 module load conda
 conda activate /projects/hpacf/mhassana/QSSPaper/conda_envs/mech_format
 source /projects/hpacf/mhassana/QSSPaper/PelePhysics/setup.sh
-rm -rf ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss_*
 
+mech_name=dodecane_lu_qss
+
+rm -rf ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/$mech_name_*
 cd ${PELE_PHYSICS_HOME}/Support/ceptr
 rm -rf log_expr
+${poetry_bin} run python generate_multi_qss.py -mf ${PELE_PHYSICS_HOME}/Support/Mechanism/Models -tm $mech_name -nm 100 
 
-${poetry_bin} run python generate_multi_qss.py -mf ${PELE_PHYSICS_HOME}/Support/Mechanism/Models -tm dodecane_lu_qss -nm 100 
 
-
-for dir in ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/dodecane_lu_qss_*/; do
+for dir in ${PELE_PHYSICS_HOME}/Support/Mechanism/Models/$mech_name_*/; do
     srun ${poetry_bin} run qssa -f ${dir}/skeletal.yaml -n ${dir}/non_qssa_list.yaml
     srun ${poetry_bin} run convert -f ${dir}/qssa.yaml --qss_format_input ${dir}/qssa_input.toml --qss_symbolic_jacobian
 done
